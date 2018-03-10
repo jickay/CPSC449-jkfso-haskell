f0cf130c64bb8dcb08c46ef565d399a3ca6d64a1
FM35 <35781412+FM35@users.noreply.github.com>
2018-03-10 12:32:33 -0700

    Create HardConstraints.hs

---
+++ src/HardConstraints.hs
@@ -0,0 +1,18 @@
+--Function can be used to determine if there is a forced partial assignment in the forbidden list
+--returns True if there isn't a forced partial assignment in the forbidden list and False otherwise
+--Takes in forced partial assignment and forbidden list as parameters
+norepeatCheck :: (Eq a) => [a] -> [a] -> Bool
+norepeatCheck [] _ = True
+norepeatCheck (x:xs) ts
+    |elem x ts = False
+    |otherwise = norepeatCheck xs ts
+
+
+--Checks if there are any repeated assignments in the forced partial assignments, which is passed in as a parameter
+--returns True if there isn't a repeat, False otherwise
+norepeatCheckself :: (Eq a) => [a] -> Bool
+norepeatCheckself [] = True
+norepeatCheckself (x:xs)
+    |elem x xs = False
+    |otherwise = norepeatCheckself xs
+
