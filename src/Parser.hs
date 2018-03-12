990f3154b8f5784cc9892972d5e476b77c7b21e3
olmorrish <35902508+olmorrish@users.noreply.github.com>
2018-03-12 00:07:27 -0600

    Added file-segmenting to Parser.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,113 +1,135 @@
-module Parser2 where
+module Parser where
 import Data.Char
 import Data.List
 
-------------------------------- FULL PARSERS -------------------------------
---getMTPairs :: [String] -> [String]
---getTTPairs :: 
+------------------------------- CONFLICT CHECKER -------------------------------
+
+--isForcedAssignmentConflict :: [(Int,Char)] -> String
+--isForcedAssignmentConflict 
 
 
---when parsing multiple tuples, **MAP** the tuple segment parsers over the [String]
+------------------------------- LINE FETCHERS -------------------------------
 
-------------------------------- TUPLE SEGMENT PARSERS -------------------------------
+fetchName :: [String] -> [String]
+fetchName x = linesBetween x "Name:" "forced partial assignment:"
 
---working
+fetchForcedPartialAssignments :: [String] -> [String]
+fetchForcedPartialAssignments x = linesBetween x "forced partial assignment:" "forbidden machine:"
+
+fetchForbiddenMachines :: [String] -> [String]
+fetchForbiddenMachines x = linesBetween x "forbidden machine:" "too-near tasks:"
+
+fetchTooNearTasks :: [String] -> [String]
+fetchTooNearTasks x = linesBetween x "too-near tasks:" "machine penalties:"
+
+fetchMachinePenalties :: [String] -> [String]
+fetchMachinePenalties x = linesBetween x "machine penalties:" "too-near penalities"
+
+fetchTooNearPenalties :: [String] -> [String]
+fetchMachinePenalties x = linesAfter x "too-near penalities"
+
+
+------------------------------- GENERAL DOCUMENT SEPARATORS -------------------------------
+
+linesAfter :: [String] -> String -> [String]
+linesAfter (x:xs) b = if ((removeEndSpaces x) == b) then xs else linesAfter xs b
+linesAfter x b = if (x!!1 == b) then [] else ["ER Error while parsing input file"] --TODO: maybe change error
+
+linesBefore :: [String] -> String -> [String]
+linesBefore (x:xs) b = if ((removeEndSpaces x) == b) then [] else x:(linesBefore xs b)
+linesBefore x b = if (x!!1 == b) then [] else ["ER Error while parsing input file"] --TODO: maybe change error
+
+linesBetween :: [String] -> String -> String -> [String] 
+linesBetween x a b = linesBefore (linesAfter x a) b  
+
+
+------------------------------- MULTILINE PARSERS -------------------------------
+
+parseMTPairs :: [String] -> [(Int, Char)]
+parseMTPairs a = map stringToMTPair a
+
+parseTTPairs :: [String] -> [(Char, Char)]
+parseTTPairs a = map stringToTTPair a
+
+--parseTTPTriples
+--parse
+
+------------------------------- SINGLE LINE PARSERS -------------------------------
+
 stringToMTPair :: String -> (Int, Char)
-stringToMTPair a = if isValidMTPair (removeEndSpaces a)
+stringToMTPair a = if (isValidMTPair (removeEndSpaces a) == "PASS")
     then (digitToInt (a !! 1), a !! 3)
     else (-1,'Z')
 
---pass a full line, returns a 
---working
 stringToTTPair :: String -> (Char, Char)
-stringToTTPair a = if isValidTTPair (removeEndSpaces a)
+stringToTTPair a = if (isValidTTPair (removeEndSpaces a) == "PASS")
     then (a !! 1, a !! 3)
     else ('Z','Z')
 
---stringToPenaltyRow :: 
+stringToTTPTriple :: String -> (Char, Char, Int)
+stringToTTPTriple a = if (isValidTTPTriple (removeEndSpaces a) == "PASS")
+    then (a !! 1, a!!3, digitToInt(a !! 5)) --TODO: that '5' only works on single digits
+    else ('Z','Z',-1)
 
-------------------------------- ERROR HANDLING -------------------------------
---checkForError :: String -> String
---checkForError (xs) = case (take 2 xs) of 
---    "ER" = drop 4 xs
---    _ = "none"
-
-
-
-
-------------------------------- INPUT SPLITTING/GETTERS -------------------------------
-
---removeSpaceLines :: [String] -> [String]
---removeSpaceLines [] = []
---removeSpaceLines (x:xs) if (allSpaces x) then removeSpaceLines xs else x:(removeSpaceLines xs)
-
---getLinesBetween :: [Char] -> [Char] -> [String]
---getLinesBetween a b = linesAfter (linesBefore b) a --"between but not including a and b"
-
---linesAfter :: [String] -> [Char] -> [String]
---linesAfter xs y = if (y `elem` xs) then drop ((y `elemIndex` xs)+1) xs else ["ER: Error while parsing input file"]
-
---linesBefore :: [String] -> [Char] -> [String]
---linesBefore xs y = if (y `elemIndex` xs) then take ((y `elemIndex` xs)+1) xs else ["ER: Error while parsing input file"]
+--
+--penaltyLineToTuple :: String -> (Int, Int, Int, Int, Int, Int, Int, Int)
+--penaltyLineToTuple ('(':a) = if ((isValidPenaltyRow a) == "PASS") then 
 
 
+------------------------------- SEGMENT VALIDATORS -------------------------------
 
+--TODO: Currently only works on single-digits in both pattern-mathing and in function usage
+isValidPenaltyRow :: [Char] -> String
+isValidPenaltyRow (a:' ':b:' ':c:' ':d:' ':e:' ':f:' ':g:' ':h:xs) = 
+    if (not (isValidPenalty a)) 
+        || (not (isValidPenalty b)) 
+        || (not (isValidPenalty c)) 
+        || (not (isValidPenalty d)) 
+        || (not (isValidPenalty e))
+        || (not (isValidPenalty f))
+        || (not (isValidPenalty g))
+        || (not (isValidPenalty h))
+    then "ER invalid penalty" else "PASS"
+isValidPenaltyRow z = "ER machine penalty error"
 
+isValidTTPTriple :: [Char] -> String
+isValidTTPTriple ['(',x,',',y,',',z,')'] = 
+    if ((isValidTask x) && (isValidTask y) && (isValidPenalty z)) then "PASS" else "ER invalid task" 
+        --TODO: See isValidPenalty func
+isValidTTPTriple z = "ER Error while parsing input file"
 
+isValidTTPair :: [Char] -> String
+isValidTTPair ['(',x,',',y,')'] = 
+    if ((isValidTask x) && (isValidTask y)) then "PASS" else "ER invalid machine/task"
+isValidTTPair z = "ER Error while parsing input file"
 
-
-
-------------------------------- PROTOTYPE FUNCTIONS -------------------------------
-
-isValidPenaltyRow :: -> String -> Bool
-isValidPenaltyRow --TODO: Pattern match to 
-
-
-
-
+isValidMTPair :: [Char] -> String
+isValidMTPair ['(',x,',',y,')'] = 
+    if ((isValidMachine x) && (isValidTask y)) then "PASS" else "ER invalid machine/task"
+isValidMTPair z = "ER Error while parsing input file"
 
 ------------------------------- WHITESPACE HANDLING -------------------------------
 
---working
 removeEndSpaces :: [Char] -> [Char]
 removeEndSpaces (x:xs) = if allSpaces xs then [x] else x:(removeEndSpaces xs)
 
---working
 allSpaces :: [Char] -> Bool
 allSpaces [] = True
 allSpaces (x:xs) = if (x == ' ') then allSpaces xs else False
 
-------------------------------- BOOL VALIDITY CHECKS -------------------------------
 
-isValidTTPTriple :: [Char] -> Bool
-isValidTTPTriple ['(',x,',',y,',',z,')'] = 
-    (isValidTask x) && (isValidTask y) && (isValidPenaltyALT z) --TODO: See isValidPenaltyALT func
-isValidTTPTriple b = False
+------------------------------- BOOL VALUE VALIDITY CHECKS -------------------------------
 
---TODO: This check may be redundant; can map if structuring penalties 
-isValidMTPTriple :: [Char] -> Bool
-isValidMTPTriple ['(',x,',',y,',',z,')'] = 
-    (isValidMachine x) && (isValidTask y) && (isValidPenaltyALT z) --TODO: See isValidPenaltyALT func
-isValidMTPTriple b = False
+--isValidPenalty :: [Char] -> Bool
+--isValidPenalty [] = False
+--isValidPenalty [x] = isDigit x
+--isValidPenalty (x:xs) = if isDigit x then isValidPenalty xs else False
 
-isValidTTPair :: [Char] -> Bool
-isValidTTPair ['(',x,',',y,')'] = ((isValidTask x) && (isValidTask y))
-isValidTTPair b = False
-
-isValidMTPair :: [Char] -> Bool
-isValidMTPair ['(',x,',',y,')'] = ((isValidMachine x) && (isValidTask y))
-isValidMTPair b = False
-
-isValidPenaltyALT :: Char -> Bool --TODO: only works on single digits
-isValidPenaltyALT a
+isValidPenalty :: Char -> Bool --TODO: only works on single digits
+isValidPenalty a
     | (b >= 0) = True
     | otherwise = False 
     where b = digitToInt a
-
-isValidPenalty :: [Char] -> Bool
-isValidPenalty [] = False
-isValidPenalty [x] = isDigit x
-isValidPenalty (x:xs) = if isDigit x then isValidPenalty xs else False
 
 isValidMachine :: Char -> Bool
 isValidMachine a 
